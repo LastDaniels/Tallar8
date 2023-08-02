4dd9ca75b8e161bf4ed9145606b4dfa18940d27d
OliLM <86312725+OliLM@users.noreply.github.com>
2023-08-02 16:17:20 -0500

    Temporario y el Main

---
+++ EmpleadoTemporario.java
@@ -0,0 +1,20 @@
+public class EmpleadoTemporario extends Empleado {
+    private String nombre;
+    private double salarioBase;
+    private int horasTrabajadas;
+    private String departamento;
+    private int mesesContrato;
+
+    public EmpleadoTemporario(String nombre, double salarioBase, int horasTrabajadas, String departamento, int mesesContrato, String genero) {
+        super(nombre,salarioBase,horasTrabajadas,0,departamento,genero);
+        this.mesesContrato = mesesContrato;
+
+    }
+
+    public void imprimirDetalles() {
+        super.imprimirDetalles();
+        System.out.println("Meses de contrato: " + mesesContrato);
+    }
+
+    // Más metodos
+}
